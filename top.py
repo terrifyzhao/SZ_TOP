04265947cc7872a85f158956a7fe2f7234163b09
joe <zjiuzhou@gmail.com>
2019-05-24 09:52:54 +0800

    Update top.py

--- top.py
+++ top.py
@@ -1,33 +1,50 @@
 import requests
 import webbrowser
+import time
 
 
-def monitor(size, ID, keyword):
-    url = 'http://hn.topsports.com.cn/phone/DataSour.aspx?ID=2&brandName=Jordan'
+def monitor(size, ID, keyword, brand, shop_id):
+    url = f'http://hn.topsports.com.cn/phone/DataSour.aspx?ID=2&brandName={brand}'
     while True:
         content = requests.get(url)
         theme_id = 0
-        if keyword in content.text:
+        if any([True for k in keyword if k in content.text]):
             themes_str = content.text.split('^')
             for theme in themes_str:
-                if keyword in theme:
+                if any([True for k in keyword if k in theme]):
                     theme_id = theme.split('‘')[1]
                     break
         if theme_id != 0:
-            get_number(theme_id, size, ID)
-            evaluate(theme_id, ID)
+            get_shop_id(theme_id)
+            get_number(theme_id, size, ID, brand, shop_id)
+            evaluate(theme_id, ID, size, keyword)
             return
 
 
-def get_number(theme, size, ID):
-    url = f'http://hn.topsports.com.cn/phone/DataSour.aspx?ID=8&zhuti={theme}&shop_id=NKSZ94&man_id={ID}&size={size}&brandName=Jordan'
+def get_number(theme, size, ID, brand, shop_id):
+    url = f'http://hn.topsports.com.cn/phone/DataSour.aspx?ID=8&zhuti={theme}&shop_id={shop_id}&man_id={ID}&size={size}&brandName={brand}'
     res = requests.get(url)
     print(res.text)
 
 
-def evaluate(theme, ID):
+def evaluate(theme, ID, size, keyword):
     url = f'http://hn.topsports.com.cn/phone/DataSour.aspx?ID=9&zhuti={theme}&man_id={ID}'
-    webbrowser.open(url)
+    res = requests.get(url)
+    if size in res.text and any([True for k in keyword if k in res.text]):
+        print('取号成功，3秒后自动打开取号页面，请再次人工确认')
+    for i in range(2, -1, -1):
+        print(f'{i + 1}...')
+        time.sleep(1)
+    webbrowser.open('http://hn.topsports.com.cn/phone/qh_select.html')
+
+
+def get_shop_id(theme):
+    url = f'http://hn.topsports.com.cn/phone/DataSour.aspx?ID=5&zhuti_id={theme}'
+    shops = requests.get(url).text.split('^')
+    print()
+    for shop in shops:
+        key, value = shop.split('‘')
+        print(key + ':' + value)
 
 
 if __name__ == '__main__':
@@ -35,6 +52,10 @@ if __name__ == '__main__':
     size = '8.5'
     # 身份证号
     ID = '530302199409126375'
-    # 关键词
-    keyword = '507'
-    monitor(size, ID, keyword)
+    # 关键词，可以填写多个
+    keyword = ['507']
+    # 品牌
+    brand = 'Jordan'
+    # shop id，默认是万象天地店，执行代码会打印所有的店的id
+    shop_id = 'NKSZ94'
+    monitor(size, ID, keyword, brand, shop_id)
